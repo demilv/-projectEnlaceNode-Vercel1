b05603b8d2f9a0e23202dd356526cd23c44b971b
demilv <gonzalo.cano@bootcamp-upgrade.com>
2023-05-08 00:31:22 +0200

    Proyecto movies1 V1.1, acabado.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {connect} = require("./src/utils/db")
 
 //Elementos necesarios para establecer la conexion a la base de datos
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 connect()
 app.use(cors())
 app.use(express.json())
